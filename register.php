a80d17268fb4bdb8bd9a0936d27e28adacfe35ab
Parsa Samadnejad <parsasam1378@gmail.com>
2019-09-25 13:14:03 +0330

    headers added

--- register.php
+++ register.php
@@ -1,5 +1,12 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+ 
+
 include_once 'config/config.php';
 global $mysqli;
 
@@ -43,6 +50,3 @@ if(
     http_response_code(400);
     echo json_encode(array("message" => "Unable to create user. Data is incomplete."));
 }
-
-
-header('Content-Type: application/json; charset=UTF-8');
