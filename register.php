6e12a0bc96fcecafbddd54aca04ce2f1e1937092
TroddenSpade <parsasam1378@gmail.com>
2019-09-26 08:58:04 +0330

    Update register.php

--- register.php
+++ register.php
@@ -2,10 +2,9 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
 header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
- 
+header("Access-Control-Allow-Headers: Origin,Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 include_once 'config/config.php';
 global $mysqli;
