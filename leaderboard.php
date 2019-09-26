6970d95555f9b0a18ce7467570f64650bc12e7cc
TroddenSpade <parsasam1378@gmail.com>
2019-09-26 19:44:46 +0330

    Create leaderboard.php

---
+++ leaderboard.php
@@ -0,0 +1,25 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Origin,Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once 'config/config.php';
+global $mysqli;
+
+$ip = (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) ? $_SERVER["HTTP_CF_CONNECTING_IP"] : $_SERVER["REMOTE_ADDR"];
+$myArray = array();
+if ($result = $mysqli->query("SELECT username,`1`,`2`,`3`,`4`,score FROM users ORDER BY score DESC, sum_time DESC")) {
+
+    while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+            $myArray[] = $row;
+    }
+    http_response_code(200);
+    echo json_encode($myArray);
+}else{
+    http_response_code(400);
+    echo json_encode(array("message" => "something went wrong"));
+            
+}
